a05af261355bf5a9c7bf2f588d9a705234b726a0
data-bomb <data-bomb@users.noreply.github.com>
2023-05-28 10:57:05 -0700

    Update Si_AutoKickNegativeKills.cs

--- Si_AutoKickNegativeKills/Si_AutoKickNegativeKills.cs
+++ Si_AutoKickNegativeKills/Si_AutoKickNegativeKills.cs
@@ -1,4 +1,29 @@
-﻿using HarmonyLib;
+/*
+ Silica Auto-Kick Negative Kills Mod
+ Copyright (C) 2023 by databomb
+ 
+ * Description *
+ For Silica listen servers, automatically identifies players who fall
+ below a certain negative kill threshold. When someone reaches the
+ threshold then players are alerted in chat, hosts are alerted in their 
+ log, and the player is kicked.
+
+ * License *
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using HarmonyLib;
 using Il2Cpp;
 using MelonLoader;
 using Si_AutoKickNegativeKills;
