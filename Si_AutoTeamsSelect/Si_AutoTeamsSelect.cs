19b40f646a3e4c5cd64c21a9f0328be980580f5c
data-bomb <data-bomb@users.noreply.github.com>
2023-05-28 10:55:53 -0700

    Update Si_AutoTeamsSelect.cs

--- Si_AutoTeamsSelect/Si_AutoTeamsSelect.cs
+++ Si_AutoTeamsSelect/Si_AutoTeamsSelect.cs
@@ -1,4 +1,28 @@
-﻿using HarmonyLib;
+/*
+ Silica Auto Teams Select Mod
+ Copyright (C) 2023 by databomb
+ 
+ * Description *
+ For Silica listen servers, automatically selects the game mode 
+ (e.g., "Humans vs. Aliens") to allow listen servers to operate
+ without constant monitoring from the host.
+
+ * License *
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using HarmonyLib;
 using Il2Cpp;
 using MelonLoader;
 using UnityEngine;
