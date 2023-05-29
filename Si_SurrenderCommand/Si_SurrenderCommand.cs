6e3a91a75fface2b9a2ffb841676f7a7ad090d45
data-bomb <data-bomb@users.noreply.github.com>
2023-05-28 17:01:49 -0700

    Update Si_SurrenderCommand.cs

--- Si_SurrenderCommand/Si_SurrenderCommand.cs
+++ Si_SurrenderCommand/Si_SurrenderCommand.cs
@@ -1,4 +1,26 @@
-﻿using HarmonyLib;
+/*
+ Silica Surrender Command Mod
+ Copyright (C) 2023 by databomb
+ 
+ * Description *
+ For Silica listen servers, provides a command (!surrender) which
+ each team's commander can use to have their team give up early.
+ * License *
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using HarmonyLib;
 using Il2Cpp;
 using Il2CppSteamworks;
 using MelonLoader;
