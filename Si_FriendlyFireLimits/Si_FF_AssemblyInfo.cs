5c39eaeda55cc59a3d0b486ca7721c68c1c3ec94
data-bomb <data-bomb@users.noreply.github.com>
2023-06-30 19:56:19 -0700

    Delete Si_FF_AssemblyInfo.cs

--- Si_FriendlyFireLimits/Si_FF_AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using MelonLoader;
-using Si_FriendlyFireLimits;
-
-[assembly: MelonInfo(typeof(FriendlyFireLimits), "Friendly Fire Limits", "1.0.0", "databomb")]
-[assembly: MelonGame("Bohemia Interactive", "Silica")]
