7035df0ddea930ef100359238cc04a4d6fe32254
data-bomb <data-bomb@users.noreply.github.com>
2023-12-17 18:22:41 -0800

    Eject v0.9.7 - Admin command to remove from vehicle
    - Adds new admin command !eject <player> to remove target player from their current vehicle

---
+++ Si_Eject/Si_Eject.cs
@@ -0,0 +1,112 @@
+﻿/*
+Silica Eject Command
+Copyright (C) 2023 by databomb
+
+* Description *
+Provides an admin command to eject a player from their current 
+vehicle.
+
+* License *
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Il2Cpp;
+using MelonLoader;
+using Si_Eject;
+using AdminExtension;
+
+[assembly: MelonInfo(typeof(Eject), "[Si] Eject Command", "0.9.7", "databomb", "https://github.com/data-bomb/Silica")]
+[assembly: MelonGame("Bohemia Interactive", "Silica")]
+
+namespace Si_Eject
+{
+    public class Eject : MelonMod
+    {
+        static bool AdminModAvailable = false;
+
+        public override void OnLateInitializeMelon()
+        {
+            AdminModAvailable = RegisteredMelons.Any(m => m.Info.Name == "Admin Mod");
+
+            if (AdminModAvailable)
+            {
+                HelperMethods.CommandCallback ejectCallback = Command_Eject;
+                HelperMethods.RegisterAdminCommand("!eject", ejectCallback, Power.Eject);
+            }
+            else
+            {
+                MelonLogger.Warning("Dependency missing: Admin Mod");
+            }
+        }
+        public void Command_Eject(Il2Cpp.Player callerPlayer, String args)
+        {
+            // validate argument count
+            int argumentCount = args.Split(' ').Length - 1;
+            if (argumentCount > 1)
+            {
+                HelperMethods.ReplyToCommand(args.Split(' ')[0] + ": Too many arguments");
+                return;
+            }
+            else if (argumentCount < 1)
+            {
+                HelperMethods.ReplyToCommand(args.Split(' ')[0] + ": Too few arguments");
+                return;
+            }
+
+            // validate argument contents
+            String sTarget = args.Split(' ')[1];
+            Il2Cpp.Player? playerToEject = HelperMethods.FindTargetPlayer(sTarget);
+
+            if (playerToEject == null)
+            {
+                HelperMethods.ReplyToCommand(args.Split(' ')[0] + ": Ambiguous or invalid target");
+                return;
+            }
+
+            if (!callerPlayer.CanAdminTarget(playerToEject))
+            {
+                HelperMethods.ReplyToCommand_Player(playerToEject, "is immune due to level");
+                return;
+            }
+
+            Unit unitOfPlayer = playerToEject.ControlledUnit;
+            if (unitOfPlayer == null)
+            {
+                HelperMethods.ReplyToCommand(args.Split(' ')[0] + ": Player does not have a vehicle");
+                return;
+            }
+
+            UnitCompartment compartmentToEject = unitOfPlayer.DriverCompartment;
+            if (compartmentToEject == null)
+            {
+                HelperMethods.ReplyToCommand(args.Split(' ')[0] + ": Player is not inside the vehicle");
+                return;
+            }
+
+            if (!compartmentToEject.IsDriver)
+            {
+                HelperMethods.ReplyToCommand(args.Split(' ')[0] + ": Player is not driving the vehicle");
+                return;
+            }    
+
+            // the DriverUnit is the original Unit that should be spawned when exiting the vehicle
+            Unit driverUnit = compartmentToEject.DriverUnit;
+            compartmentToEject.RequestRemoveUnit(driverUnit);
+
+            MelonLogger.Msg("Ejecting player (" + playerToEject.PlayerName + ") from vehicle: " + unitOfPlayer.ToString());
+
+            HelperMethods.AlertAdminActivity(callerPlayer, playerToEject, "ejected");
+        }
+    }
+}
