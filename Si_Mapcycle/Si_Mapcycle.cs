456d359960bd5d0d2d836d4498c2c989f1e4a2ca
data-bomb <data-bomb@users.noreply.github.com>
2023-12-31 12:12:13 -0800

    Mapcycle v1.1.0 - Mono support
    - Adds mono support (dedicated servers)
    - CI integration

--- Si_Mapcycle/Si_Mapcycle.cs
+++ Si_Mapcycle/Si_Mapcycle.cs
@@ -1,42 +1,46 @@
-/*
- Silica Mapcycle Mod
- Copyright (C) 2023 by databomb
- 
- * Description *
- For Silica listen servers, automatically generates a mapcycle.txt file
- and switches to the next map in the cycle at the end of each round.
+﻿/*
+Silica Map Cycle
+Copyright (C) 2023 by databomb
 
- * License *
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* Description *
+Provides map management and cycles to a server.
+
+* License *
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using HarmonyLib;
+#if NET6_0
 using Il2Cpp;
+# endif
+
+using HarmonyLib;
 using MelonLoader;
 using Si_Mapcycle;
-using UnityEngine;
 using System.Timers;
 using MelonLoader.Utils;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
-[assembly: MelonInfo(typeof(MapCycleMod), "[Si] Mapcycle", "1.0.2", "databomb", "https://github.com/data-bomb/Silica_ListenServer")]
+[assembly: MelonInfo(typeof(MapCycleMod), "Mapcycle", "1.1.0", "databomb", "https://github.com/data-bomb/Silica")]
 [assembly: MelonGame("Bohemia Interactive", "Silica")]
+[assembly: MelonOptionalDependencies("Admin Mod")]
 
 namespace Si_Mapcycle
 {
     public class MapCycleMod : MelonMod
     {
-        static String mapName = "";
         static GameMode gameModeInstance;
         static bool bEndRound;
         static bool bTimerExpired;
@@ -82,51 +86,19 @@ namespace Si_Mapcycle
             }
         }
 
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
-        {
-            mapName = sceneName;
-        }
-        public const string defaultColor = "<color=#DDE98C>";
-        public const string chatPrefix = "<b>" + defaultColor + "[<color=#DFA725>SAM" + defaultColor + "]</b> ";
-
-        //TODO change to use the admin helper methods instead
-        [HarmonyPatch(typeof(Il2CppSilica.UI.Chat), nameof(Il2CppSilica.UI.Chat.MessageReceived))]
-        private static class ApplyChatReceiveCurrentMatchInfo
-        {
-            public static void Postfix(Il2CppSilica.UI.Chat __instance, Il2Cpp.Player __0, string __1, bool __2)
-            {
-                try
-                {
-                    if (__instance.ToString().Contains("alien") && __2 == false)
-                    {
-
-                        bool isCurrMapCommand = String.Equals(__1, "!currentmap", StringComparison.OrdinalIgnoreCase);
-                        if (isCurrMapCommand)
-                        {
-                            Il2Cpp.Player serverPlayer = Il2Cpp.NetworkGameServer.GetServerPlayer();
-                            serverPlayer.SendChatMessage(chatPrefix + defaultColor + " Current map is " + String.Concat(mapName), false);
-
-                        }
-                    }
-                }
-                catch (Exception exception)
-                {
-                    string error = exception.Message;
-                    error += "\n" + exception.TargetSite;
-                    error += "\n" + exception.StackTrace;
-                    MelonLogger.Error(error);
-                }
-            }
-        }
-    private static void HandleTimerChangeLevel(object source, ElapsedEventArgs e)
+        private static void HandleTimerChangeLevel(object source, ElapsedEventArgs e)
         {
             MapCycleMod.bTimerExpired = true;
         }
 
-        [HarmonyPatch(typeof(Il2Cpp.MusicJukeboxHandler), nameof(Il2Cpp.MusicJukeboxHandler.Update))]
+        #if NET6_0
+        [HarmonyPatch(typeof(MusicJukeboxHandler), nameof(MusicJukeboxHandler.Update))]
+        #else
+        [HarmonyPatch(typeof(MusicJukeboxHandler), "Update")]
+        #endif
         private static class ApplyPatch_MusicJukeboxHandlerUpdate
         {
-            private static void Postfix(Il2Cpp.MusicJukeboxHandler __instance)
+            private static void Postfix(MusicJukeboxHandler __instance)
             {
                 // check if timer expired
                 if (MapCycleMod.bEndRound == true && MapCycleMod.bTimerExpired == true)
@@ -137,18 +109,18 @@ namespace Si_Mapcycle
 
                     String sNextMap = sMapCycle[iMapLoadCount % sMapCycle.Length];
 
-                    String sCurrentMap = Il2Cpp.NetworkGameServer.GetServerMapName();
+                    String sCurrentMap = NetworkGameServer.GetServerMap();
                     MelonLogger.Msg("Changing map to " + sNextMap + "...");
 
-                    Il2Cpp.NetworkGameServer.LoadLevel(sNextMap, MapCycleMod.gameModeInstance.GameModeInfo);
+                    NetworkGameServer.LoadLevel(sNextMap, MapCycleMod.gameModeInstance.GameModeInfo);
                 }
             }
         }
 
-        [HarmonyPatch(typeof(Il2Cpp.MusicJukeboxHandler), nameof(Il2Cpp.MusicJukeboxHandler.OnGameEnded))]
+        [HarmonyPatch(typeof(MusicJukeboxHandler), nameof(MusicJukeboxHandler.OnGameEnded))]
         private static class ApplyPatch_OnGameEnded
         {
-            public static void Postfix(Il2Cpp.MusicJukeboxHandler __instance, Il2Cpp.GameMode __0, Il2Cpp.Team __1)
+            public static void Postfix(MusicJukeboxHandler __instance, GameMode __0, Team __1)
             {
                 MapCycleMod.gameModeInstance = __0;
                 MapCycleMod.bEndRound = true;
