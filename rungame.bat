93535a57126bc3f2ecbd7eb38d8d1e3a78304307
Cornholio <143255018+Cornholio777@users.noreply.github.com>
2024-05-11 21:24:53 +0200

    CPU Performance, Asset Loading, Automatic Restart
    - Sets CPU Priority for the silica.exe to High. Sets the CPU Affinity to use CPU 0 to CPU 11, this is optimized for using 12 logical threads so for example a 12600k with 12 P-Cores, ignoring the 4 E-cores.
    - Please observe, if you have more or less P-Cores you need to adjust the CPU Affinity, see what the hex you should be using this converter:
    https://bitsum.com/tools/cpu-affinity-calculator/ 
    - The E-Cores are always the last ones in the CPU Affinity List in Task Manager. For example, 12600k P-Cores are CPU 0 to CPU 11, and E-Cores are CPU 12 to CPU 15. 
    - Additional arguments will improve asset loading times and running server at a maximum of 48 fps.
    - The script also restarts the server automatically every 24 hours.

---
+++ rungame.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+SETLOCAL EnableExtensions
+SET "EXE_PATH=Silica.exe"
+SET "ADDITIONAL_ARGS=--nogpu --batchmode --nographics --render-offscreen --screen-width 0 --screen-height 0 --fullscreen 0 --assetbundle-compression LZ4 --target-frame-rate 48 --noaudio"
+SET "MELON_ARGS=--melonloader.hideconsole --melonloader.disablestartscreen"
+:RESTART_LOOP
+start "" /B /HIGH /AFFINITY 0x0000000000000FFF %EXE_PATH% %MELON_ARGS% %ADDITIONAL_ARGS%
+@ECHO OFF
+timeout /t 86400
+taskkill /IM "Silica.exe" /F
+goto RESTART_LOOP
+ENDLOCAL
