137efa0c8be176148146a29c8bf6d5b538bddc93
data-bomb <data-bomb@users.noreply.github.com>
2024-06-30 15:20:20 -0700

    Adds a Player Surrender Vote
    - Adds a player driven surrender vote for the surrender mod
    - Commanders can still perform an immediate, unilateral surrender
    - Non-commanders can perform a vote and once ~31% of the team agrees then the team surrenders

--- Si_SurrenderCommand/Si_SurrenderCommand.cs
+++ Si_SurrenderCommand/Si_SurrenderCommand.cs
@@ -3,8 +3,10 @@
  Copyright (C) 2023-2024 by databomb
  
  * Description *
- For Silica listen servers, provides a command (!surrender) which
- each team's commander can use to have their team give up early.
+ For Silica servers, provides a command (!surrender) which each
+ each team's commander and/or players can use to have their team 
+ give up early.
+
  * License *
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -32,7 +34,7 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 
-[assembly: MelonInfo(typeof(SurrenderCommand), "Surrender Command", "1.2.7", "databomb", "https://github.com/data-bomb/Silica")]
+[assembly: MelonInfo(typeof(SurrenderCommand), "Surrender Command", "1.3.0", "databomb", "https://github.com/data-bomb/Silica")]
 [assembly: MelonGame("Bohemia Interactive", "Silica")]
 [assembly: MelonOptionalDependencies("Admin Mod")]
 
@@ -40,6 +42,17 @@ namespace Si_SurrenderCommand
 {
     public class SurrenderCommand : MelonMod
     {
+        static List<Player>[] votesToSurrender = null!;
+
+        public override void OnInitializeMelon()
+        {
+            votesToSurrender = new List<Player>[SiConstants.MaxPlayableTeams];
+            for (int i = 0; i < SiConstants.MaxPlayableTeams; i++)
+            {
+                votesToSurrender[i] = new List<Player>();
+            }
+        }
+
         public static bool IsCommander(Player thePlayer)
         {
             if (thePlayer == null)
@@ -70,6 +83,11 @@ namespace Si_SurrenderCommand
             HelperMethods.RegisterPlayerCommand("surrender", surrenderCallback, true);
         }
 
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            ClearSurrenderVotes();
+        }
+
         public static void Command_Surrender(Player? callerPlayer, String args)
         {
 			if (callerPlayer == null)
@@ -77,6 +95,14 @@ namespace Si_SurrenderCommand
 				HelperMethods.SendChatMessageToPlayer(callerPlayer, HelperMethods.chatPrefix, " Console not supported.");
 				return;
 			}
+
+            // check if they're on a valid team
+            Team? team = callerPlayer.Team;
+            if (team == null || team.Index >= SiConstants.MaxPlayableTeams)
+            {
+                HelperMethods.SendChatMessageToPlayer(callerPlayer, HelperMethods.chatPrefix, " This team does not support surrender.");
+                return;
+            }
 
             // check if game on-going
             if (!GameMode.CurrentGameMode.GameOngoing)
@@ -88,16 +114,67 @@ namespace Si_SurrenderCommand
             // check if we are actually a commander
             bool isCommander = IsCommander(callerPlayer);
 
-            if (!isCommander)
+            // if they're a commander then immediately take action
+            if (isCommander)
             {
-                // notify player on invalid usage
-                HelperMethods.ReplyToCommand_Player(callerPlayer, ": only commanders can use !surrender");
+                Surrender(team, callerPlayer);
                 return;
             }
 
-            // if they're a commander then immediately take action
-            Team surrenderTeam = callerPlayer.Team;
-            Surrender(surrenderTeam, callerPlayer);
+            // did the player already vote for surrender?
+            if (votesToSurrender[team.Index].Contains(callerPlayer))
+            {
+                HelperMethods.SendChatMessageToPlayer(callerPlayer, HelperMethods.chatPrefix, " Already voted for surrender. ", MoreSurrenderVotesNeeded(team).ToString(), " more players or 1 commander needed.");
+                return;
+            }
+
+            votesToSurrender[team.Index].Add(callerPlayer);
+
+            // if we haven't met the threshold then send a message to the teammates
+            if (votesToSurrender[team.Index].Count < TeammatesNeededForSurrender(team))
+            {
+                HelperMethods.SendChatMessageToTeam(team, HelperMethods.chatPrefix, HelperMethods.GetTeamColor(team), " ", callerPlayer.PlayerName, HelperMethods.defaultColor, " votes to surrender. ", MoreSurrenderVotesNeeded(team).ToString(), " more players or 1 commander needed.");
+                return;
+            }
+
+            Surrender(team, callerPlayer);
+        }
+
+        public static int TeammatesNeededForSurrender(Team team)
+        {
+            int playerCount = team.GetNumPlayers();
+
+            // don't count the commander as a player here
+            MP_Strategy strategyInstance = GameObject.FindObjectOfType<MP_Strategy>();
+            playerCount -= (strategyInstance.GetCommanderForTeam(team) != null ? 1 : 0);
+
+            int teammatesNeeded = (int)Math.Ceiling(playerCount * 0.31f);
+            if (teammatesNeeded < 1)
+            {
+                return 1;
+            }
+
+            return teammatesNeeded;
+        }
+
+        public static int MoreSurrenderVotesNeeded(Team team)
+        {
+            int teammatesNeeded = TeammatesNeededForSurrender(team);
+            int moreNeeded = teammatesNeeded - votesToSurrender[team.Index].Count;
+            if (moreNeeded < 1)
+            {
+                return 1;
+            }
+
+            return moreNeeded;
+        }
+
+        public static void ClearSurrenderVotes()
+        {
+            for (int i = 0; i < SiConstants.MaxPlayableTeams; i++)
+            {
+                votesToSurrender[i].Clear();
+            }
         }
 
         public static void Surrender(Team team, Player player)
@@ -113,6 +190,25 @@ namespace Si_SurrenderCommand
 
             // and destroy all units (especially the queen)
             RemoveUnits(team);
+
+            // clear all people who voted for a surrender
+            ClearSurrenderVotes();
+        }
+
+        [HarmonyPatch(typeof(MusicJukeboxHandler), nameof(MusicJukeboxHandler.OnGameStarted))]
+        private static class ApplyPatch_MusicJukeboxHandler_OnGameStarted
+        {
+            public static void Postfix(MusicJukeboxHandler __instance, GameMode __0)
+            {
+                try
+                {
+                    ClearSurrenderVotes();
+                }
+                catch (Exception error)
+                {
+                    HelperMethods.PrintError(error, "Failed to run MusicJukeboxHandler::OnGameStarted");
+                }
+            }
         }
 
         public static void RemoveConstructionSites(Team team)
