77aab4fc361fe2fb1d0438d356ac748ec5481acd
data-bomb <data-bomb@users.noreply.github.com>
2024-11-16 10:28:06 -0800

    Handle Roles with Tower Defense Mode
    - Creates generic methods that work with both MP_Strategy and MP_TowerDefense modes for capturing player role changes and whether to permit commander applicants

--- Si_AdminMod/Event_Roles.cs
+++ Si_AdminMod/Event_Roles.cs
@@ -28,6 +28,8 @@ using System.Runtime.CompilerServices;
 using System.Reflection;
 using System.Data;
 using System.Globalization;
+using static MelonLoader.MelonLogger;
+
 
 #if NET6_0
 using Il2Cpp;
@@ -41,7 +43,8 @@ namespace SilicaAdminMod
     public static class Event_Roles
     {
         #if !NET6_0
-        public static byte ERPC_RequestRole = HelperMethods.FindByteValueInEnum(typeof(MP_Strategy), "ERPCs", "REQUEST_ROLE");
+        public static byte ERPC_Strategy_RequestRole = HelperMethods.FindByteValueInEnum(typeof(MP_Strategy), "ERPCs", "REQUEST_ROLE");
+        public static byte ERPC_TowerDefense_RequestRole = HelperMethods.FindByteValueInEnum(typeof(MP_TowerDefense), "ERPCs", "REQUEST_ROLE");
         #endif
         public static event EventHandler<OnRequestCommanderArgs> OnRequestCommander = delegate { };
         public static event EventHandler<OnRoleChangedArgs> OnRoleChanged = delegate { };
@@ -62,76 +65,13 @@ namespace SilicaAdminMod
                     #if NET6_0
                     if (__1 != (byte)MP_Strategy.ERPCs.REQUEST_ROLE)
                     #else
-                    if (__1 != ERPC_RequestRole)
+                    if (__1 != ERPC_Strategy_RequestRole)
                     #endif
                     {
                         return true;
                     }
 
-                    Player requestingPlayer = Player.FindPlayer((NetworkID)__0.ReadUInt64(), (int)__0.ReadByte());
-                    GameModeExt.ETeamRole eRole = (GameModeExt.ETeamRole)__0.ReadByte();
-
-                    if (requestingPlayer == null)
-                    {
-                        MelonLogger.Warning("Cannot find player in role request.");
-                        return false;
-                    }
-
-                    // would the game code treat it as an infantry/no role request?
-                    if (eRole != GameModeExt.ETeamRole.COMMANDER || __instance.GetCommanderForTeam(requestingPlayer.Team))
-                    {
-                        __0 = RestoreRPC_RequestRoleReader(requestingPlayer, eRole);
-                        FireOnRoleChangedEvent(requestingPlayer, eRole);
-                        return true;
-                    }
-
-                    BaseTeamSetup baseTeamSetup = __instance.GetTeamSetup(requestingPlayer.Team);
-                    if (baseTeamSetup == null)
-                    {
-                        return false;
-                    }
-
-                    OnRequestCommanderArgs onRequestCommanderArgs = FireOnRequestCommanderEvent(requestingPlayer);
-
-                    if (onRequestCommanderArgs.Block)
-                    {
-                        if (SiAdminMod.Pref_Admin_DebugLogMessages.Value)
-                        {
-                            MelonLogger.Msg("Blocking commander role request for " + onRequestCommanderArgs.Requester.PlayerName);
-                        }
-                        
-                        if (!onRequestCommanderArgs.PreventSpawnWhenBlocked)
-                        {
-                            if (SiAdminMod.Pref_Admin_DebugLogMessages.Value)
-                            {
-                                MelonLogger.Msg("Preventing Spawn");
-                            }
-                            __instance.SpawnUnitForPlayer(requestingPlayer, requestingPlayer.Team);
-                            FireOnRoleChangedEvent(requestingPlayer, GameModeExt.ETeamRole.INFANTRY);
-                        }
-
-                        return false;
-                    }
-
-                    if (SiAdminMod.Pref_Admin_DebugLogMessages.Value)
-                    {
-                        MelonLogger.Msg("Allowing to join commander");
-                    }
-#if NET6_0
-                    __instance.SetCommander(baseTeamSetup.Team, requestingPlayer);
-                    __instance.RPC_SynchCommander(baseTeamSetup.Team);
-#else
-                    Type strategyType = typeof(MP_Strategy);
-                    MethodInfo setCommanderMethod = strategyType.GetMethod("SetCommander", BindingFlags.Instance | BindingFlags.NonPublic);
-                    setCommanderMethod.Invoke(__instance, parameters: new object?[] { baseTeamSetup.Team, requestingPlayer });
-
-                    MethodInfo synchCommanderMethod = strategyType.GetMethod("RPC_SynchCommander", BindingFlags.Instance | BindingFlags.NonPublic);
-                    synchCommanderMethod.Invoke(__instance, new object[] { baseTeamSetup.Team });
-#endif
-
-                    FireOnRoleChangedEvent(requestingPlayer, GameModeExt.ETeamRole.COMMANDER);
-
-                    return false;
+                    return ProcessRequestRole(__instance, ref __0, __1);
                 }
                 catch (Exception error)
                 {
@@ -142,16 +82,154 @@ namespace SilicaAdminMod
             }
         }
 
-        public static GameByteStreamReader RestoreRPC_RequestRoleReader(Player requestingPlayer, GameModeExt.ETeamRole role)
+        [HarmonyPatch(typeof(MP_TowerDefense), nameof(MP_TowerDefense.ProcessNetRPC))]
+        static class ApplyPatch_MPTowerDefense_RequestRole
+        {
+            public static bool Prefix(MP_TowerDefense __instance, ref GameByteStreamReader __0, byte __1)
+            {
+                try
+                {
+                    if (__instance == null || __0 == null)
+                    {
+                        return true;
+                    }
+
+                    // only look at RPC_RequestRole
+                    #if NET6_0
+                    if (__1 != (byte)MP_TowerDefense.ERPCs.REQUEST_ROLE)
+                    #else
+                    if (__1 != ERPC_TowerDefense_RequestRole)
+                    #endif
+                    {
+                        return true;
+                    }
+
+                    return ProcessRequestRole(__instance, ref __0, __1);
+                }
+                catch (Exception error)
+                {
+                    HelperMethods.PrintError(error, "Failed to run MP_TowerDefense::ProcessNetRPC");
+                }
+
+                return true;
+            }
+        }
+
+        public static bool ProcessRequestRole<T>(T gameModeInstance, ref GameByteStreamReader reader, byte rpcIndex) where T : GameModeExt
+        {
+            Player requestingPlayer = Player.FindPlayer((NetworkID)reader.ReadUInt64(), (int)reader.ReadByte());
+            GameModeExt.ETeamRole eRole = (GameModeExt.ETeamRole)reader.ReadByte();
+
+            if (requestingPlayer == null)
+            {
+                MelonLogger.Warning("Cannot find player in role request.");
+                return false;
+            }
+
+            // would the game code treat it as an infantry/no role request?
+            if (eRole != GameModeExt.ETeamRole.COMMANDER || gameModeInstance.GetCommanderForTeam(requestingPlayer.Team))
+            {
+                reader = RestoreRPC_RequestRoleReader(gameModeInstance, requestingPlayer, eRole);
+                FireOnRoleChangedEvent(requestingPlayer, eRole);
+                return true;
+            }
+
+            BaseTeamSetup? baseTeamSetup = GetTeamSetup(gameModeInstance, requestingPlayer.Team);
+            if (baseTeamSetup == null)
+            {
+                return false;
+            }
+
+            OnRequestCommanderArgs onRequestCommanderArgs = FireOnRequestCommanderEvent(requestingPlayer);
+
+            if (onRequestCommanderArgs.Block)
+            {
+                if (SiAdminMod.Pref_Admin_DebugLogMessages.Value)
+                {
+                    MelonLogger.Msg("Blocking commander role request for " + onRequestCommanderArgs.Requester.PlayerName);
+                }
+
+                if (!onRequestCommanderArgs.PreventSpawnWhenBlocked)
+                {
+                    if (SiAdminMod.Pref_Admin_DebugLogMessages.Value)
+                    {
+                        MelonLogger.Msg("Preventing Spawn");
+                    }
+                    gameModeInstance.SpawnUnitForPlayer(requestingPlayer, requestingPlayer.Team);
+                    FireOnRoleChangedEvent(requestingPlayer, GameModeExt.ETeamRole.INFANTRY);
+                }
+
+                return false;
+            }
+
+            if (SiAdminMod.Pref_Admin_DebugLogMessages.Value)
+            {
+                MelonLogger.Msg("Allowing to join commander");
+            }
+
+            #if NET6_0
+            if (gameModeInstance is MP_Strategy strategyInstance)
+            {
+                strategyInstance.SetCommander(baseTeamSetup.Team, requestingPlayer);
+                strategyInstance.RPC_SynchCommander(baseTeamSetup.Team);
+            }
+            else if (gameModeInstance is MP_TowerDefense defenseInstance)
+            { 
+                defenseInstance.SetCommander(baseTeamSetup.Team, requestingPlayer);
+                defenseInstance.RPC_SynchCommander(baseTeamSetup.Team);
+            }
+            #else
+            Type gameModeType = gameModeInstance.GetType();
+            MethodInfo setCommanderMethod = gameModeType.GetMethod("SetCommander", BindingFlags.Instance | BindingFlags.NonPublic);
+            setCommanderMethod.Invoke(gameModeInstance, parameters: new object?[] { baseTeamSetup.Team, requestingPlayer });
+
+            MethodInfo synchCommanderMethod = gameModeType.GetMethod("RPC_SynchCommander", BindingFlags.Instance | BindingFlags.NonPublic);
+            synchCommanderMethod.Invoke(gameModeInstance, new object[] { baseTeamSetup.Team });
+            #endif
+
+            FireOnRoleChangedEvent(requestingPlayer, GameModeExt.ETeamRole.COMMANDER);
+
+            return false;
+        }
+
+        public static BaseTeamSetup? GetTeamSetup<T>(T gameModeInstance, Team team) where T : GameModeExt
+        {
+            foreach (BaseTeamSetup baseTeamSetup in gameModeInstance.BaseTeamSetups)
+            {
+                if (gameModeInstance.GetTeamSetupActive(baseTeamSetup) && baseTeamSetup.Team == team)
+                {
+                    return baseTeamSetup;
+                }
+            }
+
+            return null;
+        }
+
+        public static GameByteStreamReader RestoreRPC_RequestRoleReader<T>(T gameModeInstance, Player requestingPlayer, GameModeExt.ETeamRole role) where T : GameModeExt
         {
             GameByteStreamWriter gameByteStreamWriter = GameByteStreamWriter.GetGameByteStreamWriter(0U, "Si_AdminMod::RestoreRPC_RequestRoleReader", true);
             gameByteStreamWriter.WriteByte((byte)ENetworkPacketType.GameModeRPC);
             gameByteStreamWriter.WriteByte(0);
-            #if NET6_0
-            gameByteStreamWriter.WriteByte((byte)MP_Strategy.ERPCs.REQUEST_ROLE);
-            #else
-            gameByteStreamWriter.WriteByte(ERPC_RequestRole);
-            #endif
+            //#if NET6_0
+            if (gameModeInstance is MP_Strategy)
+            {
+                #if NET6_0
+                gameByteStreamWriter.WriteByte((byte)MP_Strategy.ERPCs.REQUEST_ROLE);
+                #else
+                gameByteStreamWriter.WriteByte((byte)ERPC_Strategy_RequestRole);
+                #endif
+            }
+            else if (gameModeInstance is MP_TowerDefense)
+            {
+                #if NET6_0
+                gameByteStreamWriter.WriteByte((byte)MP_TowerDefense.ERPCs.REQUEST_ROLE);
+                #else
+                gameByteStreamWriter.WriteByte((byte)ERPC_TowerDefense_RequestRole);
+                #endif
+            }
+            //#else
+            //gameByteStreamWriter.WriteByte(ERPC_Strategy_RequestRole);
+            //#endif
             gameByteStreamWriter.WriteUInt64((ulong)requestingPlayer.PlayerID);
             gameByteStreamWriter.WriteByte((byte)requestingPlayer.PlayerChannel);
             gameByteStreamWriter.WriteByte((byte)role);
