d58908ce77a07b2801bccfb71778a70898dbbe79
data-bomb <data-bomb@users.noreply.github.com>
2024-12-19 16:15:48 -0800

    Commander Management v0.8.115 Fixes
    - Moves commander lottery to happen immediately before the round starts to avoid new server messages that say AI has taken command
    - Note: This does not eliminate duplicate promotion messages. Additional work may be needed to suppress the server chat messages if it's a nuisance or eliminate the mod's promotion message

--- Si_CommManagement/CommanderApplications.cs
+++ Si_CommManagement/CommanderApplications.cs
@@ -136,136 +136,173 @@ namespace Si_CommanderManagement
             return true;
         }
 
-        [HarmonyPatch(typeof(MusicJukeboxHandler), nameof(MusicJukeboxHandler.OnGameStarted))]
-        private static class ApplyPatchOnGameStarted
+        #if NET6_0
+        [HarmonyPatch(typeof(MP_Strategy), nameof(MP_Strategy.OnMissionStateChanged))]
+        #else
+        [HarmonyPatch(typeof(MP_Strategy), "OnMissionStateChanged")]
+        #endif
+        private static class ApplyPatch_Strategy_OnMissionStateChanged
         {
-            public static void Postfix(MusicJukeboxHandler __instance, GameMode __0)
+            public static void Prefix(MP_Strategy __instance, MP_Strategy.EMissionState __0)
             {
                 try
                 {
-                    Mutineer.ClearMutineerList();
-
-                    if (CommanderApplications.commanderApplicants == null || CommanderApplications.previousCommanders == null || promotedCommanders == null)
+                    // run just before round starts
+                    if (__0 == MP_Strategy.EMissionState.STARTED)
                     {
-                        return;
-                    }
-
-                    System.Random randomIndex = new System.Random();
-                    Player? RemovePlayer = null;
-
-                    for (int i = 0; i < SiConstants.MaxPlayableTeams; i++)
-                    {
-                        // clear previous commander tracking status, if any
-                        if (teamswapCommanderChecks != null)
-                        {
-                            teamswapCommanderChecks[i] = null;
-                        }
-
-                        if (Team.Teams[i] == null)
-                        {
-                            // account for transitions from 2 to 3 team rounds
-                            commanderApplicants[i].Clear();
-                            continue;
-                        }
-
-                        if (commanderApplicants[i].Count == 0)
-                        {
-                            continue;
-                        }
-
-                        // remove previous commanders from applicant list, if there is a choice
-                        if (commanderApplicants[i].Count > 1)
-                        {
-                            for (int j = 0; j < previousCommanders.Count; j++)
-                            {
-                                RemovePlayer = commanderApplicants[i].Find(k => k == previousCommanders[j].Commander);
-                                if (RemovePlayer != null)
-                                {
-                                    MelonLogger.Msg("Removing applicant from 2 rounds ago from random selection: " + RemovePlayer.PlayerName);
-                                    GameMode.CurrentGameMode.SpawnUnitForPlayer(RemovePlayer, RemovePlayer.Team);
-                                    commanderApplicants[i].Remove(RemovePlayer);
-                                }
-                            }
-
-                            if (commanderApplicants[i].Count == 0)
-                            {
-                                continue;
-                            }
-                        }
-
-                        int iCommanderIndex = randomIndex.Next(0, commanderApplicants[i].Count - 1);
-                        Player CommanderPlayer = commanderApplicants[i][iCommanderIndex];
-
-                        if (CommanderPlayer != null && CommanderPlayer.Team.Index == i)
-                        {
-                            HelperMethods.ReplyToCommand("Promoted " + HelperMethods.GetTeamColor(CommanderPlayer) + CommanderPlayer.PlayerName + "</color> to commander for " + HelperMethods.GetTeamColor(CommanderPlayer) + CommanderPlayer.Team.TeamShortName + "</color>");
-                            promotedCommanders[CommanderPlayer.Team.Index] = CommanderPlayer;
-                            CommanderPrimitives.PromoteToCommander(CommanderPlayer);
-                            PreviousCommander prevCommander = new PreviousCommander()
-                            {
-                                Commander = CommanderPlayer,
-                                RoundsLeft = 2
-                            };
-                            previousCommanders.Add(prevCommander);
-                            commanderApplicants[i].RemoveAt(iCommanderIndex);
-                        }
-                        else
-                        {
-                            MelonLogger.Warning("Can't find lottery winner. Not promoting for team " + Team.Teams[i].TeamShortName);
-                        }
-
-                        // switch remaining players to infantry
-                        foreach (Player infantryPlayer in commanderApplicants[i])
-                        {
-                            if (infantryPlayer == null)
-                            {
-                                continue;
-                            }
-
-                            MelonLogger.Msg("Player " + infantryPlayer.PlayerName + " lost commander lottery. Spawning as infantry.");
-                            GameMode.CurrentGameMode.SpawnUnitForPlayer(infantryPlayer, infantryPlayer.Team);
-                        }
-
-                        // everyone is promoted or moved to infantry, clear for the next round
-                        commanderApplicants[i].Clear();
-                        MelonLogger.Msg("Clearing commander lottery for team " + Team.Teams[i].TeamShortName);
-                    }
-
-                    // we want to remove the oldest commanders from the list, if we have any
-                    if (previousCommanders == null || previousCommanders.Count == 0)
-                    {
-                        MelonLogger.Msg("Skipping clearing previous commanders. Nothing to do.");
-                        return;
-                    }
-
-                    List<PreviousCommander>? stalePreviousCommanders = new List<PreviousCommander>();
-                    foreach (PreviousCommander previousCommander in previousCommanders)
-                    {
-                        previousCommander.RoundsLeft -= 1;
-                        if (previousCommander.Commander == null)
-                        {
-                            stalePreviousCommanders.Add(previousCommander);
-                            MelonLogger.Msg("Adding stale, invalid commander.");
-                            continue;
-                        }
-
-                        if (previousCommander.RoundsLeft <= 0)
-                        {
-                            stalePreviousCommanders.Add(previousCommander);
-                            MelonLogger.Msg("Adding stale commander with 0 rounds left: " + previousCommander.Commander.PlayerName);
-                        }
-                    }
-
-                    foreach (PreviousCommander stalePreviousCommander in stalePreviousCommanders)
-                    {
-                        previousCommanders.Remove(stalePreviousCommander);
-                        MelonLogger.Msg("Removed stale commander.");
+                        HoldCommanderLottery();
                     }
                 }
                 catch (Exception error)
                 {
-                    HelperMethods.PrintError(error, "Failed to run MusicJukeboxHandler::OnGameStarted");
+                    HelperMethods.PrintError(error, "Failed to run MP_Strategy::OnMissionStateChanged");
                 }
+            }
+        }
+
+        #if NET6_0
+        [HarmonyPatch(typeof(MP_TowerDefense), nameof(MP_TowerDefense.OnMissionStateChanged))]
+        #else
+        [HarmonyPatch(typeof(MP_TowerDefense), "OnMissionStateChanged")]
+        #endif
+        private static class ApplyPatch_TowerDefense_OnMissionStateChanged
+        {
+            public static void Prefix(MP_TowerDefense __instance, MP_TowerDefense.EMissionState __0)
+            {
+                try
+                {
+                    // run just before round starts
+                    if (__0 == MP_TowerDefense.EMissionState.STARTED)
+                    {
+                        HoldCommanderLottery();
+                    }
+                }
+                catch (Exception error)
+                {
+                    HelperMethods.PrintError(error, "Failed to run MP_TowerDefense::OnMissionStateChanged");
+                }
+            }
+        }
+
+        public static void HoldCommanderLottery()
+        {
+            Mutineer.ClearMutineerList();
+
+            if (CommanderApplications.commanderApplicants == null || CommanderApplications.previousCommanders == null || promotedCommanders == null)
+            {
+                return;
+            }
+
+            System.Random randomIndex = new System.Random();
+            Player? RemovePlayer = null;
+
+            for (int i = 0; i < SiConstants.MaxPlayableTeams; i++)
+            {
+                // clear previous commander tracking status, if any
+                if (teamswapCommanderChecks != null)
+                {
+                    teamswapCommanderChecks[i] = null;
+                }
+
+                if (Team.Teams[i] == null)
+                {
+                    // account for transitions from 2 to 3 team rounds
+                    commanderApplicants[i].Clear();
+                    continue;
+                }
+
+                if (commanderApplicants[i].Count == 0)
+                {
+                    continue;
+                }
+
+                // remove previous commanders from applicant list, if there is a choice
+                if (commanderApplicants[i].Count > 1)
+                {
+                    for (int j = 0; j < previousCommanders.Count; j++)
+                    {
+                        RemovePlayer = commanderApplicants[i].Find(k => k == previousCommanders[j].Commander);
+                        if (RemovePlayer != null)
+                        {
+                            MelonLogger.Msg("Removing applicant from 2 rounds ago from random selection: " + RemovePlayer.PlayerName);
+                            GameMode.CurrentGameMode.SpawnUnitForPlayer(RemovePlayer, RemovePlayer.Team);
+                            commanderApplicants[i].Remove(RemovePlayer);
+                        }
+                    }
+
+                    if (commanderApplicants[i].Count == 0)
+                    {
+                        continue;
+                    }
+                }
+
+                int iCommanderIndex = randomIndex.Next(0, commanderApplicants[i].Count - 1);
+                Player CommanderPlayer = commanderApplicants[i][iCommanderIndex];
+
+                if (CommanderPlayer != null && CommanderPlayer.Team.Index == i)
+                {
+                    HelperMethods.ReplyToCommand("Promoted " + HelperMethods.GetTeamColor(CommanderPlayer) + CommanderPlayer.PlayerName + "</color> to commander for " + HelperMethods.GetTeamColor(CommanderPlayer) + CommanderPlayer.Team.TeamShortName + "</color>");
+                    promotedCommanders[CommanderPlayer.Team.Index] = CommanderPlayer;
+                    CommanderPrimitives.PromoteToCommander(CommanderPlayer);
+                    PreviousCommander prevCommander = new PreviousCommander()
+                    {
+                        Commander = CommanderPlayer,
+                        RoundsLeft = 2
+                    };
+                    previousCommanders.Add(prevCommander);
+                    commanderApplicants[i].RemoveAt(iCommanderIndex);
+                }
+                else
+                {
+                    MelonLogger.Warning("Can't find lottery winner. Not promoting for team " + Team.Teams[i].TeamShortName);
+                }
+
+                // switch remaining players to infantry
+                foreach (Player infantryPlayer in commanderApplicants[i])
+                {
+                    if (infantryPlayer == null)
+                    {
+                        continue;
+                    }
+
+                    MelonLogger.Msg("Player " + infantryPlayer.PlayerName + " lost commander lottery. Spawning as infantry.");
+                    GameMode.CurrentGameMode.SpawnUnitForPlayer(infantryPlayer, infantryPlayer.Team);
+                }
+
+                // everyone is promoted or moved to infantry, clear for the next round
+                commanderApplicants[i].Clear();
+                MelonLogger.Msg("Clearing commander lottery for team " + Team.Teams[i].TeamShortName);
+            }
+
+            // we want to remove the oldest commanders from the list, if we have any
+            if (previousCommanders == null || previousCommanders.Count == 0)
+            {
+                MelonLogger.Msg("Skipping clearing previous commanders. Nothing to do.");
+                return;
+            }
+
+            List<PreviousCommander>? stalePreviousCommanders = new List<PreviousCommander>();
+            foreach (PreviousCommander previousCommander in previousCommanders)
+            {
+                previousCommander.RoundsLeft -= 1;
+                if (previousCommander.Commander == null)
+                {
+                    stalePreviousCommanders.Add(previousCommander);
+                    MelonLogger.Msg("Adding stale, invalid commander.");
+                    continue;
+                }
+
+                if (previousCommander.RoundsLeft <= 0)
+                {
+                    stalePreviousCommanders.Add(previousCommander);
+                    MelonLogger.Msg("Adding stale commander with 0 rounds left: " + previousCommander.Commander.PlayerName);
+                }
+            }
+
+            foreach (PreviousCommander stalePreviousCommander in stalePreviousCommanders)
+            {
+                previousCommanders.Remove(stalePreviousCommander);
+                MelonLogger.Msg("Removed stale commander.");
             }
         }
 
